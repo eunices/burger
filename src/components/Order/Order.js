95034a5ac15ddd10abe6f6234dc94b53a8f8e219
Eunice Soh <eunice.sjy@gmail.com>
2021-04-17 17:18:30 +0800

    [EJYS] Make calls to retrieve orders

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -3,10 +3,28 @@ import React from 'react';
 import classes from  './Order.module.css';
 
 const order = (props) => {
+
+  const ingredientsFormatted = Object.keys(props.ingredients).map(
+    ingredient => {
+      return(
+        <span 
+          key={ingredient}
+          style={{
+            textTransform: 'capitalize',
+            display: 'inline-block',
+            margin: '0px 8px',
+            border: '1px solid #ccc',
+            padding: '5px',
+          }}>
+          {`${ingredient} (${props.ingredients[ingredient]})`}
+        </span>
+      );},
+  );
+
   return(
     <div className={classes.Order}>
-      <p>Ingredients: Salad (1)</p>
-      <p>Price: <strong>SGD 5.80</strong></p>
+      <p>Ingredients: {ingredientsFormatted}</p>
+      <p>Price: <strong>SGD {props.totalPrice.toFixed(2)}</strong></p>
     </div>
   );
 };
--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -3,14 +3,49 @@ import React, {Component} from 'react';
 import classes from './Orders.modules.css';
 
 import Order from '../../components/Order/Order';
+import Spinner from '../../components/UI/Spinner/Spinner';
+import ax from '../../axios-orders';
 
 class Orders extends Component { 
+
+  state = {
+    loading: false,
+    orders: [],
+  }
+
+  componentDidMount() {
+    this.setState({loading: true});
+
+    ax.get('/orders.json')
+      .then(response => {
+        const orders = Object.keys(response.data)
+          .map(key => {
+            return {...response.data[key], key: key};
+          });
+        this.setState({loading: false, orders: orders});
+      })
+      .catch(_error => {
+        this.setState({loading: false});
+      });
+  }
+
   render() {
+
+    const ordersShown = this.state.loading ? 
+      <Spinner/> :
+      (this.state.orders.map((order, i) => {
+        return(
+          <Order
+            key={`order${i}`}
+            ingredients={order.ingredients}
+            totalPrice={order.price}
+          />
+        );
+      }));
+
     return(
       <div className={classes.Orders}>
-        <Order/>
-        <Order/>
-        <Order/>
+        {ordersShown}
       </div>
     );
   }
